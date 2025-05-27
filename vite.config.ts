27037e5e381e043efff7c3e7e3aa1dadb9911973
WalkQuackBack <quacksareback@duck.com>
2025-05-26 22:12:28 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: "/m3-expressive-morph-shape/
+  base: '/m3-expressive-morph-shape/'
 })
